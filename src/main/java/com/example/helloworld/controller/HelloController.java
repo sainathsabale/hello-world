a9adcf30163e4e20bbea0c133e10210f249a0035
sainathsabale <sainath.sabale@gmail.com>
2020-10-17 20:12:19 +0530

    Added getName method

--- src/main/java/com/example/helloworld/controller/HelloController.java
+++ src/main/java/com/example/helloworld/controller/HelloController.java
@@ -1,5 +1,13 @@
 package com.example.helloworld.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
-    
+
+    @GetMapping("/getName")
+    public String getName(){
+        return "";
+    }
 }
